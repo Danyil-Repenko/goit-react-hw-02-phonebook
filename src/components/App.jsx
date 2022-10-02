4c19b60d43b4d3cab0f6ac0f75a44c92146ba867
Danyil-Repenko <danyil.oficial@gmail.com>
2022-10-02 18:50:30 +0300

    крок 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,39 @@
 import React, { Component } from 'react';
 import { nanoid } from 'nanoid';
 import { Contacts } from './Contacts/Contacts';
+import { Filter } from './Filter/Filter';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     name: '',
+    number: '',
+    filter: '',
   };
 
   addContact = e => {
     e.preventDefault();
+    const nameInput = e.target.elements.name;
+    const numberInput = e.target.elements.number;
+
     const contact = {
-      name: e.target.elements.name.value,
+      name: nameInput.value,
+      number: numberInput.value,
       id: nanoid(),
     };
-    this.setState(
-      state => {
-        if (!state.contacts.includes(contact))
-          return state.contacts.push(contact);
-      },
-      () => console.log(this.state)
-    );
+
+    this.setState(state => {
+      if (!state.contacts.includes(contact))
+        return state.contacts.push(contact);
+    });
+
+    nameInput.value = '';
+    numberInput.value = '';
   };
 
   render() {
@@ -38,10 +51,21 @@ export class App extends Component {
               required
             />
           </label>
+          <label>
+            Number
+            <input
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
           <button type="submit">Add contact</button>
         </form>
         <div>
           <h2>Contacts</h2>
+          <Filter />
           <Contacts contacts={this.state.contacts} />
         </div>
       </div>
--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,6 +1,8 @@
 export const Contacts = ({ contacts }) => {
   const creatContacts = contacts.map(contact => (
-    <li key={contact.id}>{contact.name}</li>
+    <li key={contact.id}>
+      {contact.name}: {contact.number}
+    </li>
   ));
   return <ul>{creatContacts}</ul>;
 };
